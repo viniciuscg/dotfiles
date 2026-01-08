2978baa2e5b36a7f94589c6023a5c007d6d8c853
hypermnesia <viniciuscgoncalves@hotmail.com>
2026-01-07 21:01:16 -0300

    feat: update polybar configuration for 4K monitor support and add small bar variants

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,10 +4,19 @@ killall -q polybar
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload top &
-    MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload bottom &
+    if [ "$m" = "DP-1-5" ]; then
+      # Monitor 4K principal - barras maiores
+      echo "Monitor 4K principal - barras maiores"
+      MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload top &
+      MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload bottom &
+    else
+      echo "Monitores menores (DP-1-6 e eDP-1) - barras menores"
+      MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload top-small &
+      MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload bottom-small &
+    fi
   done
 else
+  echo "Monitor único - barras maiores"
   polybar -c ~/.config/polybar/config.ini --reload top &
   polybar -c ~/.config/polybar/config.ini --reload bottom &
 fi
