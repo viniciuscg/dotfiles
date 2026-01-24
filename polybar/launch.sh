f504d5ed8abbd354ad25efc40c4e0d962f23f623
viniciuscg <viniciuscgoncalves@hotmail.com>
2026-01-23 21:13:14 -0300

    feat: new config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,1 +1,24 @@
-/home/viniciusgoncalves/project/dotfiles/polybar/launch.sh
+#!/bin/bash
+
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    if [ "$m" = "DP-1-5" ]; then
+      # Monitor 4K principal - barras maiores
+      echo "Monitor 4K principal - barras maiores"
+      MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload top &
+      MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload bottom &
+    else
+      echo "Monitores menores (DP-1-6 e eDP-1) - barras menores"
+      MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload top-small &
+      MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload bottom-small &
+    fi
+  done
+else
+  echo "Monitor único - barras maiores"
+  polybar -c ~/.config/polybar/config.ini --reload top &
+  polybar -c ~/.config/polybar/config.ini --reload bottom &
+fi
+
+echo "Polybar launched..."
