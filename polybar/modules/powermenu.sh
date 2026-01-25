1353957faabe8b4c8155377be67c83a49aab4a86
hypermnesia <viniciuscgoncalves@hotmail.com>
2026-01-25 16:06:46 -0300

    new config

--- polybar/modules/powermenu.sh
+++ polybar/modules/powermenu.sh
@@ -1,34 +1,30 @@
 #!/usr/bin/bash
 # ~/.config/polybar/modules/powermenu.sh
 
-# Opções do menu com ícones minimalistas
-OPTIONS="  Reiniciar\n  Desligar\n  Hibernar\n󰜺  Cancelar"
+OPTIONS="  Reiniciar\n  Desligar\n  Hibernar\n  Cancelar"
 
-SELECTED=$(echo -e "$OPTIONS" | rofi -dmenu -i -p "Power" -theme-str 'window {width: 250px;}')
+SELECTED=$(echo -e "$OPTIONS" | rofi -dmenu -i -p "Power Menu" -theme-str 'window {width: 300px;}')
 
 case "$SELECTED" in
     *"Reiniciar")
-        CONFIRM=$(echo -e "Sim\nNão" | rofi -dmenu -i -p "Reiniciar?" -theme-str 'window {width: 200px;}')
+        CONFIRM=$(echo -e "Sim\nNão" | rofi -dmenu -i -p "Confirmar reiniciar?")
         if [[ "$CONFIRM" == "Sim" ]]; then
-            notify-send -u critical -t 3000 "Sistema" "Reiniciando..."
-            sleep 1
             systemctl reboot
         fi
         ;;
     *"Desligar")
-        CONFIRM=$(echo -e "Sim\nNão" | rofi -dmenu -i -p "Desligar?" -theme-str 'window {width: 200px;}')
+        CONFIRM=$(echo -e "Sim\nNão" | rofi -dmenu -i -p "Confirmar desligar?")
         if [[ "$CONFIRM" == "Sim" ]]; then
-            notify-send -u critical -t 3000 "Sistema" "Desligando..."
-            sleep 1
             systemctl poweroff
         fi
         ;;
     *"Hibernar")
-        CONFIRM=$(echo -e "Sim\nNão" | rofi -dmenu -i -p "Hibernar?" -theme-str 'window {width: 200px;}')
+        CONFIRM=$(echo -e "Sim\nNão" | rofi -dmenu -i -p "Confirmar hibernar?")
         if [[ "$CONFIRM" == "Sim" ]]; then
-            notify-send -t 3000 "Sistema" "Hibernando..."
-            sleep 1
             systemctl hibernate
         fi
+        ;;
+    *)
+        exit 0
         ;;
 esac
